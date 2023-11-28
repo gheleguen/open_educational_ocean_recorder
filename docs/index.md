bbe5d8db9727e2c507f3eb95d78fef51cd36ff62
ghe.leguen <ghe.leguen@gmail.com>
2023-11-28 12:39:47 +0100

    Premier commit

---
+++ docs/index.md
@@ -0,0 +1,29 @@
+# Un enregistreur de la température de fond
+
+
+## 1/ Toutes les clés pour se lancer
+
+### 1.1 Télécharger l'archive
+
+Avant toute chose, [télécharger l'archive du projet](https://github.com/gheleguen/porte_a_poules/archive/refs/heads/main.zip). C'est dans ce dossier que se trouve tous les fichiers de documantations mais aussi les fichiers de découpes ou les modèles d'impression 3D. 
+
+
+**Ou passer par Git Hub**
+
+Se rendre dans le [git hub](https://github.com/gheleguen/open_educational_ocean_recorder.git) et télécharger l'archive complète du projet. 
+
+**Ou en lignes de commandes**
+
+ - Ouvrir un terminal linux,
+ - Si ce n'est pas déjà fait installer ```git``` : <pre>sudo apt install git</pre>
+ - Se placer dans le dossier de votre ordinateur où vous souhaitez installer enregistrer l'archive.
+ - Cloner l'archive : <pre>git clone https://github.com/gheleguen/open_educational_ocean_recorder.git</pre> 
+
+
+### 1.2 L'arborescence de l'archive
+
+ - **docs :** Est le dossier qui contient les documents et images qui constituent la documentation.  Ce sont des fichiers marckdown (.md) que l'on peut ouvrir avec un éditeur de texte.
+
+ - **Hardware :** Le dossier qui contient les fichiers d'impressions 3D, de découpe laser...  
+
+ - **Software :** Le dossier qui contient le code à flasher sur l'arduino.
---
+++ software/dev_mot_et_rtc/dev_mot_et_rtc.ino
@@ -0,0 +1,108 @@
+#include <Wire.h>  
+#include "DS3231.h"
+
+RTClib RTC;
+DS3231 Clock;
+
+//Paramètres de déclenchements de la porte
+int Houv = 11; // Heure d'ouverture
+int Mouv = 30; // Minute d'ouverture
+int Hferm = 11; //Heure de fermeture
+int Mferm = 31; // Minute de fermeture
+
+// Branchements
+int enA = 4; 
+int in1 = 3;
+int in2 = 2;
+int pin_buttonA = 7; // port numérique lié au bouton poussoir 1
+int pin_buttonB = 8; // port numérique lié au bouton poussoir 2
+
+//Introduction de variables
+int Year;
+int Month;
+int Date;
+int Hour;
+int Minute;
+int Second;
+int interval = 1;
+int Minute_last;
+int Date_last;
+int i = 0;
+
+
+void setup()
+{
+  //Initialise le moniteur série
+  Serial.begin(9600);
+  Wire.begin(); //for DS3231 for RTC
+  
+  // Defini tous les ports du contr^leur moteur comme des sorties
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  analogWrite(enA, 255); 
+  DateTime now = RTC.now();
+  Minute = now.minute();
+}
+ 
+void loop()
+{  
+  delay(1000);
+  Serial.println();
+  Serial.print(Year);
+  Serial.print("/");
+  Serial.print(Month);
+  Serial.print("/");
+  Serial.print(Date);
+  Serial.print(" ");
+  Serial.print(Hour);
+  Serial.print(":");
+  Serial.print(Minute);
+  Serial.print(":");
+  Serial.print(Second);
+  if (Hour >= Houv && Hour < Hferm && i == 0) { // Si l'heure est supérieur à l'heure minimale d'ouverture et que la porte est fermé
+      ouverture(); // Ouverture de la porte
+   }
+  if (Hour >= Hferm && Hour > Houv && i == 1) { // Si l'heure est supérieur à l'heure minimale de fermeture et que la porte est fermé
+      fermeture(); // Ouverture de la porte
+   }
+  DateTime now = RTC.now();
+  Year = now.year();
+  Month = now.month();
+  Date = now.day();
+  Hour = now.hour();
+  Minute = now.minute();
+  Second = now.second();
+}
+
+void ouverture(){
+  Serial.println();
+  Serial.print("Ouverture de la porte...");
+  while (digitalRead(pin_buttonA) == HIGH){ // Tant que le bouton est en position High, le moteur tourne
+      //le moteur tourne
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+  }
+  Serial.println();
+  Serial.print("Porte ouverte ;-)");
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  i = 1; // I defini l'etat d'ouverture de la porte, ici i prend la valeur 1 ce qui signifie que la porte est ouverte
+  delay(1000);
+}
+
+void fermeture(){
+  Serial.println();
+  Serial.print("Fermeture de la porte...");
+  while (digitalRead(pin_buttonB) == HIGH){ // Tant que le bouton est en position High, le moteur tourne
+      //le moteur tourne
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+  }
+  Serial.println();
+  Serial.print("Porte ferme ;-)");
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  i = 0;
+  delay(1000);
+}
